68c8aec16b8dbb4985def8f28857174b0bf9a3fd
KianJanbozorgi <kian84.c16@gmail.com>
2023-09-03 22:12:07 +0330

    cleaning and fixing button

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt5.QtWidgets import QWidget , QApplication , QLabel , QPushButton
 
 app = QApplication([])
-btn = QPushButton()
+
 window = QWidget()
+
+btn = QPushButton("click me!" , parent=window)
 window.setWindowTitle("My dear application!")
 window.show()
 
