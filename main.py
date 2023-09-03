552d55af55e04fa68654d47a4f8637585a65b0d2
KianJanbozorgi <kian84.c16@gmail.com>
2023-09-03 22:10:29 +0330

    added button

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QWidget , QApplication , QLabel
+from PyQt5.QtWidgets import QWidget , QApplication , QLabel , QPushButton
 
 app = QApplication([])
-
+btn = QPushButton()
 window = QWidget()
 window.setWindowTitle("My dear application!")
 window.show()
